e4a6255e08f38df4aa31416fb4a88782dea6b9c9
shippokun <okashi0tabetai@gmail.com>
2021-04-22 01:11:20 +0900

    refactor(*): convert composition-api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -119,12 +119,17 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { defineComponent } from "vue";
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
